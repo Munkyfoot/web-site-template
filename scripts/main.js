4b9e5d88d0d34cc7b4ba29b8cecdf2bbe80ad7d5
Munkyfoot <eazyfoot@gmail.com>
2019-07-18 19:45:18 -0700

    Make divs move with mouse and fix border-box styling

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,29 @@ $(function () {
     $('body').mousemove(function (event) {
         var pageCoords = "" + event.pageX + "px 100vh";
         $(this).css('background', "radial-gradient(circle at " + pageCoords + ", white, rgb(224,224,224))");
+        var pageWidth = $(document).width();
+        var pageHeight = $(document).height();
+        var _posModX = (event.pageX / pageWidth - 0.5) * 10;
+
+        $('.half, .portrait').each(function () {
+            var yDistance = Math.abs(event.pageY - $(this).position().top) / pageHeight;
+            posModX = _posModX * (1 - yDistance);
+            var baseMargin = pageWidth * 0.5 - $(this).outerWidth() * 0.5
+            $(this).css('marginLeft', (baseMargin - posModX) + 'px');
+            $(this).css('marginRight', (baseMargin + posModX) + 'px');
+        });
+        $('body > div:not(.navbar, .half)').each(function () {
+            var yDistance = Math.abs(event.pageY - $(this).position().top) / pageHeight;
+            posModX = _posModX * (1 - yDistance);
+            $(this).css('marginLeft', (10 - posModX) + 'px');
+            $(this).css('marginRight', (10 + posModX) + 'px');
+        });
+        $('body > *:not(div, img)').each(function () {
+            var yDistance = Math.abs(event.pageY - $(this).position().top) / pageHeight;
+            posModX = _posModX * (1 - yDistance);
+            $(this).css('paddingLeft', (10 - posModX) + 'px');
+            $(this).css('paddingRight', (10 + posModX) + 'px');
+        });
     });
     SequentialFade();
 });
