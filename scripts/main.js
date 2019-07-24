dc28a63fbd311e54c8ae495cf1f7ba8fbd48ef51
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2019-07-24 01:14:24 -0700

    Zoom on portrait on hover

--- scripts/main.js
+++ scripts/main.js
@@ -1,33 +1,24 @@
 $(function () {
+    $('.portrait').each(function () {
+        var midX = ($(this).children('img').width() - $(this).width()) * 0.5;
+        var midY = ($(this).children('img').height() - $(this).height()) * 0.5;
+        $(this).scrollLeft(midX);
+        $(this).scrollTop(midY);
+    });
+
     $('body').mousemove(function (event) {
         var pageCoords = "" + event.pageX + "px 100vh";
         $(this).css('background', "radial-gradient(circle at " + pageCoords + ", white, rgb(224,224,224))");
         var pageWidth = $(document).width();
         var pageHeight = $(document).height();
-        //var _posModX = (event.pageX / pageWidth - 0.5) * 10;
-        /*
-        $('.half, .portrait').each(function () {
-            var yDistance = Math.abs(event.pageY - $(this).position().top) / pageHeight;
-            posModX = _posModX * (1 - yDistance);
-            var baseMargin = pageWidth * 0.5 - $(this).outerWidth() * 0.5
-            $(this).css('marginLeft', (baseMargin - posModX) + 'px');
-            $(this).css('marginRight', (baseMargin + posModX) + 'px');
-        });
-        $('body > div:not(.navbar, .half)').each(function () {
-            var yDistance = Math.abs(event.pageY - $(this).position().top) / pageHeight;
-            posModX = _posModX * (1 - yDistance);
-            $(this).css('marginLeft', (10 - posModX) + 'px');
-            $(this).css('marginRight', (10 + posModX) + 'px');
-        });
-        */
-        $('body > *:not(div, img), .navbar').each(function () {
+        $('body > *:not(div):not(img), .navbar').each(function () {
             var yPosition = $(this).position().top + $(this).outerHeight() * 0.5;
             var yDistance = Math.abs(event.pageY - yPosition) / pageHeight;
             var posModX = (event.pageX / pageWidth - 0.5) * 10 * Math.pow(1 - yDistance, 10);
             $(this).css('paddingLeft', (10 - posModX) + 'px');
             $(this).css('paddingRight', (10 + posModX) + 'px');
         });
-        $('div:not(.navbar) > *:not(div, img)').each(function () {
+        $('div:not(.navbar) > *:not(div):not(img)').each(function () {
             var xPosition = $(this).position().left;
             var xDistance = Math.abs(event.pageX - xPosition) / pageWidth;
             var yPosition = $(this).position().top + $(this).outerHeight() * 0.5;
