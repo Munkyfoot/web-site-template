c9ed4611b5d81dfe8d3a0e58eaf55adcdc2cdfe7
Munkyfoot <17400831+Munkyfoot@users.noreply.github.com>
2019-07-25 00:12:59 -0700

    Indicate current page in navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,4 +1,5 @@
 $(function () {
+    var curPage = window.location.pathname.split('/').slice(-1)[0];
     var pages = {
         'Home': 'index.html',
         'About': 'about.html',
@@ -6,7 +7,11 @@ $(function () {
     };
     var links = "";
     for (var page in pages) {
-        links += "<a href='" + pages[page] + "'>" + page + "</a>";
+        var tag = "";
+        if(pages[page] == curPage){
+            tag = "id='cur_page'";
+        }
+        links += "<a href='" + pages[page] + "'" + tag + ">" + page + "</a>";
     }
     $('body').prepend("<div class='navbar'>" + links + "<span id='header'>Template</span></div>");
     SequentialFade('.navbar, .navbar > *', 100);
